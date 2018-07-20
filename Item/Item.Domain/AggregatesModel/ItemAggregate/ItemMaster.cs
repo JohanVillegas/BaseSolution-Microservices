eb70e2bf544d08ef00f63b900f8eff05494f360e
Johan Villegas <johanvillegas.mj@hotmail.com>
2018-07-20 17:56:04 -0400

    Implementando AddItemMaster

--- Item/Item.Domain/AggregatesModel/ItemAggregate/ItemMaster.cs
+++ Item/Item.Domain/AggregatesModel/ItemAggregate/ItemMaster.cs
@@ -8,14 +8,37 @@ namespace Item.Domain.AggregatesModel.ItemAggregate
     public class ItemMaster :Entity, IAggregateRoot
     {
         #region [ attributes ]
-        public string Number { get; set; }
-        public string Name { get; set; }
-        public string ShortName { get; set; }
-        public string Decription { get; set; }
-        public UnitMeasure UnitMeasure { get; set; }
-        public ItemMasterType Type { get; set; }
-        public DateTime DateTime { get; set; }
-        public bool Active { get; set; }
+        public string Number { get; private set; }
+        public string Name { get; private set; }
+        public string ShortName { get; private set; }
+        public string Decription { get; private set; }
+        public DateTime DateTime { get; private set; }
+        public bool Active { get; private set; }
+
+        public UnitMeasure UnitMeasure { get; private set; }
+        public ItemMasterType Type { get; private set; }
+        #endregion
+
+        #region [ methods ]
+        public static ItemMaster AddItemMaster(string number, string name, string shortName, string description, 
+                                               DateTime dateTime, bool active, UnitMeasure unitMeasure, ItemMasterType type)
+        {
+            ItemMaster itemMaster = new ItemMaster()
+            {
+                Id = Guid.NewGuid(),
+                Number = number,
+                Name = name,
+                ShortName = shortName,
+                Decription = description,
+                DateTime = dateTime,
+                Active = active,
+                UnitMeasure = unitMeasure,
+                Type = type
+            };
+
+            return itemMaster;
+        }
+
         #endregion
 
         #region [ notas de la clases ]
