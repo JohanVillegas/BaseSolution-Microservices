77c1d74e2259b978e4c247eccbae35bfefcd5bde
Johan Villegas <johanvillegas.mj@hotmail.com>
2018-09-13 16:20:05 -0400

    Agregando Clase Exception y un implementacion en ItemMaster

---
+++ src/Services/Item/Item.Domain/Exceptions/ItemDomainException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Item.Domain.Exceptions
+{
+    /// <summary>
+    /// Exception type for domain exceptions
+    /// </summary>
+    public class ItemDomainException : Exception
+    {
+        public ItemDomainException()
+        { }
+
+        public ItemDomainException(string message)
+            : base(message)
+        { }
+
+        public ItemDomainException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
