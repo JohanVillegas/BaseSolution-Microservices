a21c782a7b5ed0c603e1ce47514d0572a33fd487
Johan Villegas <johanvillegas.mj@hotmail.com>
2018-09-13 16:21:15 -0400

    Creacion de un seccion de infraestructura para controlar la excepciones que viene desde el dominio y implmentando el servicio en Statup

---
+++ src/Services/Item/Item.API/Infrastructure/HttpGlobalExceptionFilter.cs
@@ -0,0 +1,67 @@
+﻿using Item.API.Infrastructure.ActionResults;
+using Item.Domain.Exceptions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Item.API.Infrastructure
+{
+    public class HttpGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly IHostingEnvironment env;
+
+        public HttpGlobalExceptionFilter(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception.GetType() == typeof(ItemDomainException))
+            {
+                var problemDetails = new ValidationProblemDetails()
+                {
+                    Instance = context.HttpContext.Request.Path,
+                    Status = StatusCodes.Status400BadRequest,
+                    Detail = "Please refer to the errors property for additional details."
+                };
+
+                problemDetails.Errors.Add("DomainValidations", new string[] { context.Exception.Message.ToString() });
+
+                context.Result = new BadRequestObjectResult(problemDetails);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                var json = new JsonErrorResponse
+                {
+                    Messages = new[] { "An error occur.Try it again." }
+                };
+
+                if (env.IsDevelopment())
+                {
+                    json.DeveloperMessage = context.Exception;
+                }
+                // Result asigned to a result object but in destiny the response is empty. This is a known bug of .net core 1.1
+                // It will be fixed in .net core 1.1.2. See https://github.com/aspnet/Mvc/issues/5594 for more information
+                context.Result = new InternalServerErrorObjectResult(json);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            }
+            context.ExceptionHandled = true;
+        }
+
+        private class JsonErrorResponse
+        {
+            public string[] Messages { get; set; }
+
+            public object DeveloperMessage { get; set; }
+        }
+    }
+}
